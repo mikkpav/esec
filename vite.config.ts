7dacb7d25cdda2dce5484c78f66b9ec123bda9d5
mikkpav <mikk.pavelson@gmail.com>
2026-04-17 10:05:16 +0300

    Removed /esec base from vite.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    base: '/esec',
 	plugins: [
 		react(), 
 		tailwindcss()
